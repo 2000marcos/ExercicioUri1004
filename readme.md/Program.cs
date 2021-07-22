b7c70657688c10abe6dc1a5c7ce1b677f71ca021
perkles <souza.marcosv2@gmail.com>
2021-07-22 16:26:36 -0300

    primeiro commit

---
+++ readme.md/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace readme.md
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, produto;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+
+            produto = A * B;
+
+            Console.WriteLine("PROD = " + produto);
+
+        }
+    }
+}
